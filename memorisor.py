3403dfeb36a5c7d9f46ed9aa004711434f5e3a3d
Yoosy12121 <8ahmedyusuf8@gmail.com>
2025-11-08 10:57:09 +0000

    Add files via upload

--- memorisor.py
+++ memorisor.py
@@ -1,11 +1,59 @@
 from tkinter import*
 from tkinter.filedialog import*
 
+def adding():
+    text=entryblock.get()
+    if len(text.strip())>0:
+        list.insert(END,text)
+
+def remove():
+    index=list.curselection()
+    print(index)
+    index=index[ : :-1]
+    if index:
+        for i in index:
+            list.delete(i)
+
+def save():
+    saveas=asksaveasfile(filetypes=[("Text Document","*.txt")])
+    if saveas is not None:
+        for item in list.get(0,END):
+            print(item,file=saveas)
+        list.delete(0,END)
+
+def enter():
+    file=askopenfile()
+    if file is not None:
+        content=file.readlines()
+        for item in content:
+            list.insert(END,item)
 mainscreen=Tk()
 mainscreen.geometry("500x250")
 
-savebutton=Button(mainscreen,text="Save",font=("arial",15))
+savebutton=Button(mainscreen,text="Save",font=("arial",15),command=save)
 savebutton.pack(side="top")
+
+entryblock=Entry(mainscreen,text="",)
+entryblock.pack(side="top")
+
+addbutton=Button(mainscreen,text="Add",command=adding)
+addbutton.pack(side="top")
+
+scrollwheel=Scrollbar(mainscreen)
+scrollwheel.pack(side="right",fill=Y)
+
+list=Listbox(mainscreen,yscrollcommand=scrollwheel.set,selectmode=MULTIPLE)
+for i in range (10):
+    list.insert(END,str(i)+"hi")
+list.pack(side="top")
+
+openbutton=Button(mainscreen,text="Open",command=enter)
+openbutton.place(x=80,y=100)
+
+deleteButton=Button(mainscreen,text="Delete",command=remove)
+deleteButton.place(x=420,y=90)
+
+
 
 
 
--- pizza.py
+++ pizza.py
@@ -3,10 +3,44 @@ from tkinter.ttk import*
 
 mainscreen=Tk()
 mainscreen.geometry("500x200")
-names=["cat","dog","fish"]
+
+heading=Label(mainscreen,text="Welcome to Pizza Hut",font=("arial",15))
+heading.place(x=150,y=0)
+
+pizzaselection=Label(mainscreen,text="Select your pizza",font=("arial",10))
+pizzaselection.place(x=70, y=50)
+pizzas=["Margherita"," Pepperoni","Hawaiian"]
 choice=StringVar()
-option=OptionMenu(mainscreen,choice,names)
-option.pack(side="left")
+option=OptionMenu(mainscreen,choice,pizzas)
+option.place(x=190,y=49)
+
+enterquantity=Label(mainscreen,text="Enter quantity",font=("arial",10))
+enterquantity.place(x=71,y=80)
+
+numberofpizzas=Combobox(mainscreen,text="0")
+numberofpizzas.place(x=180,y=80)
+numberofpizzas["values"]=tuple(range(1,1000))
+
+orderbutton=Button(mainscreen,text="Order")
+orderbutton.place(x=220,y=120)
+
+chosenrd=IntVar()
+
+
+rs = Radiobutton(mainscreen, text="S",value="S",variable=chosenrd)
+rs.place(x=450, y=50)
+
+rm = Radiobutton(mainscreen, text="M",value="M",variable=chosenrd)
+rm.place(x=450, y=80)
+
+rl = Radiobutton(mainscreen, text="L",value="l",variable=chosenrd)
+rl.place(x=450, y=110)
+
+chosenrd.set(S)
+
+
+
+
 
 
 
